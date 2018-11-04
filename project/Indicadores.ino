4b4c4030c5ef8f1f537183f4b76995be7e63d6db
Matheus Amancio <matheusamancio23@hotmail.com>
2018-11-03 21:06:30 -0300

    finished logic

--- project/Indicadores.ino
+++ project/Indicadores.ino
@@ -1,1 +1,71 @@
 ////
+
+void  apagaInd(int pos) {
+  switch (pos) {
+    case 1:
+      digitalWrite(i03, HIGH);
+      break;
+    case 2:
+      digitalWrite(i03, HIGH);
+      break;
+    case 3:
+      digitalWrite(i03, HIGH);
+      break;
+    case 4:
+      digitalWrite(i03, HIGH);
+      break;
+    case 5:
+      digitalWrite(i03, HIGH);
+      break;
+    case 6:
+      digitalWrite(i03, HIGH);
+      break;
+    case 7:
+      digitalWrite(i03, HIGH);
+      break;
+  }
+}
+
+  void indica(int pos, int & a, int & b, int & c) {
+    switch (pos) {
+      case 1:
+        digitalWrite(a, LOW);
+        digitalWrite(b, LOW);
+        digitalWrite(c, LOW);
+        break;
+      case 2:
+        digitalWrite(a, HIGH);
+        digitalWrite(b, LOW);
+        digitalWrite(c, LOW);
+        break;
+      case 3:
+        digitalWrite(a, LOW);
+        digitalWrite(b, LOW);
+        digitalWrite(c, LOW);
+        break;
+      case 4:
+        digitalWrite(a, HIGH);
+        digitalWrite(b, LOW);
+        digitalWrite(c, LOW);
+        break;
+      case 5:
+        digitalWrite(a, LOW);
+        digitalWrite(b, HIGH);
+        digitalWrite(c, LOW);
+        break;
+      case 6:
+        digitalWrite(a, LOW);
+        digitalWrite(b, HIGH);
+        digitalWrite(c, LOW);
+        break;
+      case 7:
+        digitalWrite(a, HIGH);
+        digitalWrite(b, HIGH);
+        digitalWrite(c, LOW);
+        break;
+      default:
+        digitalWrite(a, HIGH);
+        digitalWrite(b, HIGH);
+        digitalWrite(c, LOW);
+    }
+  }
--- project/project.ino
+++ project/project.ino
@@ -1,56 +1,117 @@
 #include "Metro.h"
 
 Metro metro1 = Metro(500);
+Metro metro2 = Metro(500);
 
 //Enderecos Elevador A
-int a0 = 38;
-int a1 = 39;
-int a2 = 40;
+int a0 = 22;
+int a1 = 23;
+int a2 = 24;
 int ia = 0;
 
+//Enderecos Elevador B
+int b0 = 25;
+int b1 = 26;
+int b2 = 27;
+int ib = 0;
+
+//Enderecos Elevador C
+int b0 = 28;
+int b1 = 29;
+int b2 = 30;
+int ib = 0;
+
+//Enderecos Elevador D
+int b0 = 31;
+int b1 = 32;
+int b2 = 33;
+int ib = 0;
+
+
+
+//Indicadores
+int i01 = 3;
+int i02 = 2;
+int i03 = 4;
 
 // botões
 int a = 5;
 int b = 6;
 int c = 7;
 int s = 8;
-int fila1[20];
-int nfila1 = 0;
+
+
+int filaA[20];
+int filaB[20];
+int nfilaA = 0;
+int nfilaB = 0;
 void setup() {
   Serial.begin(9600);
   //Enderecos Elevador A
   pinMode(a0, OUTPUT);
   pinMode(a1, OUTPUT);
   pinMode(a2, OUTPUT);
+  
+  //Enderecos Elevador B
+  pinMode(b0, OUTPUT);
+  pinMode(b1, OUTPUT);
+  pinMode(b2, OUTPUT);
+  
+  
   // botões
   pinMode(a, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(c, OUTPUT);
   pinMode(s, INPUT);
+
+  // botões
+  pinMode(i01, OUTPUT);
+  pinMode(i02, OUTPUT);
+  pinMode(i03, OUTPUT);
+  digitalWrite(i03, HIGH);
 }
 
 
 void loop() {
 
   int a00, a01, a02, b00, b01, b02;
+  
+  
+    if (metro2.check()) {
+    if (filaB[0] == -1) {
+      Serial.print("deu -1");
+    } else if (ib < filaB[0]) {
+      ib++;
+    }
+    else if (ib > filaB[0]) {
+      ib--;
+    } else {
+      realocamento(filaB, ib);
+    }
+    changePosition(ib, b00, b01, b02);
+    digitalWrite(b0, b00);
+    digitalWrite(b1, b01);
+    digitalWrite(b2, b02);
+  }
+  
+  
 
   if (metro1.check()) {
-    if (fila1[0] == -1) {
+    if (filaA[0] == -1) {
       Serial.print("deu -1");
-    } else if (ia < fila1[0]) {
+    } else if (ia < filaA[0]) {
       ia++;
     }
-    else if (ia > fila1[0]) {
+    else if (ia > filaA[0]) {
       ia--;
     } else {
-      realocamento(fila1);
+      realocamento(filaA, ia);
     }
     changePosition(ia, a00, a01, a02);
     digitalWrite(a0, a00);
     digitalWrite(a1, a01);
     digitalWrite(a2, a02);
   }
-
 
 
   // botões
@@ -63,36 +124,34 @@ void loop() {
     if (digitalRead(s) == HIGH) {
       int arr[3] = {c1, b1, a1};
       if (arr[0] == LOW && arr[1] == LOW && arr[2] == LOW) {
-        //        Serial.print("0");
-        addFila(fila1, nfila1, 1);
+        addFila(filaA, nfilaA, 1);
         break;
       } else if (arr[0] == LOW && arr[1] == LOW && arr[2] == HIGH) {
-        //        Serial.print("1");
-        addFila(fila1, nfila1, 2);
+        addFila(filaB, nfilaB, 2);
         break;
       } else if (arr[0] == LOW && arr[1] == HIGH && arr[2] == LOW) {
-        //        Serial.print("2");
-        addFila(fila1, nfila1, 3);
+        addFila(filaA, nfilaA, 3);
         break;
       } else if (arr[0] == LOW && arr[1] == HIGH && arr[2] == HIGH) {
-        //        Serial.print("3");
-        addFila(fila1, nfila1, 4);
+        addFila(filaB, nfilaB, 4);
         break;
       } else if (arr[0] == HIGH && arr[1] == LOW && arr[2] == LOW) {
-        //        Serial.print("4");
-        addFila(fila1, nfila1, 5);
+        addFila(filaA, nfilaA, 5);
         break;
       } else if (arr[0] == HIGH && arr[1] == LOW && arr[2] == HIGH) {
-        //        Serial.print("5");
-        addFila(fila1, nfila1, 6);
+        addFila(filaA, nfilaA, 6);
         break;
       } else if (arr[0] == HIGH && arr[1] == HIGH && arr[2] == LOW) {
-        //        Serial.print("6");
-        addFila(fila1, nfila1, 7);
+        
+        digitalWrite(i01, HIGH);
+        digitalWrite(i02, HIGH);
+        digitalWrite(i03, LOW);
+        addFila(filaA, nfilaA, 7);
+
         break;
       } else
         //        Serial.print("7");
-        addFila(fila1, nfila1, 0);
+        addFila(filaA, nfilaA, 0);
       break;
     }
   }
